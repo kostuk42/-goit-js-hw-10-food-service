ded9a81827915beba2a0c3540135ffee16ca61ae
kostuk42 <kostuk42@i.ua>
2021-05-20 10:17:46 +0300

    final point

--- dist/src.0d2e5ed2.js
+++ dist/src.0d2e5ed2.js
@@ -117,7 +117,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../node_modules/handlebars/dist/handlebars.runtime.js":[function(require,module,exports) {
+})({"pAws":[function(require,module,exports) {
 var define;
 var global = arguments[3];
 /**!
@@ -2132,7 +2132,7 @@ THE SOFTWARE.
 });
 
 ;
-},{}],"templates/templateMenu.hbs":[function(require,module,exports) {
+},{}],"AkgI":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -2259,7 +2259,7 @@ var templateFunction = _handlebars.default.template({
 
 var _default = templateFunction;
 exports.default = _default;
-},{"handlebars/dist/handlebars.runtime":"../node_modules/handlebars/dist/handlebars.runtime.js"}],"menu.json":[function(require,module,exports) {
+},{"handlebars/dist/handlebars.runtime":"pAws"}],"mA8c":[function(require,module,exports) {
 module.exports = [{
   "id": "XWaQXcbk0",
   "name": "Картофель, запеченный в мундире",
@@ -2317,7 +2317,7 @@ module.exports = [{
   "price": 240,
   "ingredients": ["Круглый рис", "Мини цукини", "Тертый имбирь", "Грибы шиитаке", "Соевый соус", "Кунжутное масло"]
 }];
-},{}],"index.js":[function(require,module,exports) {
+},{}],"Focm":[function(require,module,exports) {
 "use strict";
 
 var _templateMenu = _interopRequireDefault(require("./templates/templateMenu.hbs"));
@@ -2326,14 +2326,14 @@ var _menu = _interopRequireDefault(require("./menu.json"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-console.log(_templateMenu.default);
-console.log(_menu.default);
+// rendering menu tamplates to HTML
 var placeToInsert = document.querySelector('.js-menu');
 
 _menu.default.forEach(function (elemMenu) {
   var markup = (0, _templateMenu.default)(elemMenu);
   placeToInsert.insertAdjacentHTML('beforeend', markup);
-});
+}); // switching dark/light themes
+
 
 var Theme = {
   LIGHT: 'light-theme',
@@ -2341,228 +2341,25 @@ var Theme = {
 };
 var switcherElem = document.querySelector('.theme-switch__toggle');
 var currentTheme = localStorage.getItem('currentTheme');
+var bodyClassElem = document.body.classList;
+
+var onChange = function onChange(e) {
+  bodyClassElem.toggle(Theme.DARK);
+  bodyClassElem.toggle(Theme.LIGHT);
+  localStorage.setItem('currentTheme', bodyClassElem[0]);
+};
 
 if (currentTheme === null) {
   localStorage.setItem('currentTheme', Theme.LIGHT);
-  document.body.classList.add(Theme.LIGHT);
+  bodyClassElem.add(Theme.LIGHT);
 } else {
-  document.body.classList.add(currentTheme);
+  bodyClassElem.add(currentTheme);
 }
 
 if (currentTheme === Theme.DARK) {
   switcherElem.checked = true;
 }
 
-var onChange = function onChange(e) {
-  document.body.classList.toggle(Theme.DARK);
-  document.body.classList.toggle(Theme.LIGHT);
-  localStorage.setItem('currentTheme', document.body.classList[0]);
-};
-
 switcherElem.addEventListener('change', onChange);
-},{"./templates/templateMenu.hbs":"templates/templateMenu.hbs","./menu.json":"menu.json"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
-var global = arguments[3];
-var OVERLAY_ID = '__parcel__error__overlay__';
-var OldModule = module.bundle.Module;
-
-function Module(moduleName) {
-  OldModule.call(this, moduleName);
-  this.hot = {
-    data: module.bundle.hotData,
-    _acceptCallbacks: [],
-    _disposeCallbacks: [],
-    accept: function (fn) {
-      this._acceptCallbacks.push(fn || function () {});
-    },
-    dispose: function (fn) {
-      this._disposeCallbacks.push(fn);
-    }
-  };
-  module.bundle.hotData = null;
-}
-
-module.bundle.Module = Module;
-var checkedAssets, assetsToAccept;
-var parent = module.bundle.parent;
-
-if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
-  var hostname = "" || location.hostname;
-  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64321" + '/');
-
-  ws.onmessage = function (event) {
-    checkedAssets = {};
-    assetsToAccept = [];
-    var data = JSON.parse(event.data);
-
-    if (data.type === 'update') {
-      var handled = false;
-      data.assets.forEach(function (asset) {
-        if (!asset.isNew) {
-          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
-
-          if (didAccept) {
-            handled = true;
-          }
-        }
-      }); // Enable HMR for CSS by default.
-
-      handled = handled || data.assets.every(function (asset) {
-        return asset.type === 'css' && asset.generated.js;
-      });
-
-      if (handled) {
-        console.clear();
-        data.assets.forEach(function (asset) {
-          hmrApply(global.parcelRequire, asset);
-        });
-        assetsToAccept.forEach(function (v) {
-          hmrAcceptRun(v[0], v[1]);
-        });
-      } else if (location.reload) {
-        // `location` global exists in a web worker context but lacks `.reload()` function.
-        location.reload();
-      }
-    }
-
-    if (data.type === 'reload') {
-      ws.close();
-
-      ws.onclose = function () {
-        location.reload();
-      };
-    }
-
-    if (data.type === 'error-resolved') {
-      console.log('[parcel] ✨ Error resolved');
-      removeErrorOverlay();
-    }
-
-    if (data.type === 'error') {
-      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
-      removeErrorOverlay();
-      var overlay = createErrorOverlay(data);
-      document.body.appendChild(overlay);
-    }
-  };
-}
-
-function removeErrorOverlay() {
-  var overlay = document.getElementById(OVERLAY_ID);
-
-  if (overlay) {
-    overlay.remove();
-  }
-}
-
-function createErrorOverlay(data) {
-  var overlay = document.createElement('div');
-  overlay.id = OVERLAY_ID; // html encode message and stack trace
-
-  var message = document.createElement('div');
-  var stackTrace = document.createElement('pre');
-  message.innerText = data.error.message;
-  stackTrace.innerText = data.error.stack;
-  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
-  return overlay;
-}
-
-function getParents(bundle, id) {
-  var modules = bundle.modules;
-
-  if (!modules) {
-    return [];
-  }
-
-  var parents = [];
-  var k, d, dep;
-
-  for (k in modules) {
-    for (d in modules[k][1]) {
-      dep = modules[k][1][d];
-
-      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
-        parents.push(k);
-      }
-    }
-  }
-
-  if (bundle.parent) {
-    parents = parents.concat(getParents(bundle.parent, id));
-  }
-
-  return parents;
-}
-
-function hmrApply(bundle, asset) {
-  var modules = bundle.modules;
-
-  if (!modules) {
-    return;
-  }
-
-  if (modules[asset.id] || !bundle.parent) {
-    var fn = new Function('require', 'module', 'exports', asset.generated.js);
-    asset.isNew = !modules[asset.id];
-    modules[asset.id] = [fn, asset.deps];
-  } else if (bundle.parent) {
-    hmrApply(bundle.parent, asset);
-  }
-}
-
-function hmrAcceptCheck(bundle, id) {
-  var modules = bundle.modules;
-
-  if (!modules) {
-    return;
-  }
-
-  if (!modules[id] && bundle.parent) {
-    return hmrAcceptCheck(bundle.parent, id);
-  }
-
-  if (checkedAssets[id]) {
-    return;
-  }
-
-  checkedAssets[id] = true;
-  var cached = bundle.cache[id];
-  assetsToAccept.push([bundle, id]);
-
-  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
-    return true;
-  }
-
-  return getParents(global.parcelRequire, id).some(function (id) {
-    return hmrAcceptCheck(global.parcelRequire, id);
-  });
-}
-
-function hmrAcceptRun(bundle, id) {
-  var cached = bundle.cache[id];
-  bundle.hotData = {};
-
-  if (cached) {
-    cached.hot.data = bundle.hotData;
-  }
-
-  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
-    cached.hot._disposeCallbacks.forEach(function (cb) {
-      cb(bundle.hotData);
-    });
-  }
-
-  delete bundle.cache[id];
-  bundle(id);
-  cached = bundle.cache[id];
-
-  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
-    cached.hot._acceptCallbacks.forEach(function (cb) {
-      cb();
-    });
-
-    return true;
-  }
-}
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/src.e31bb0bc.js.map
+},{"./templates/templateMenu.hbs":"AkgI","./menu.json":"mA8c"}]},{},["Focm"], null)
+//# sourceMappingURL=/-goit-js-hw-10-food-service/src.0d2e5ed2.js.map
--- src/index.js
+++ src/index.js
@@ -1,34 +1,31 @@
+// rendering menu tamplates to HTML
 import templateMenu from './templates/templateMenu.hbs';
-console.log(templateMenu);
 import actualMenu from './menu.json';
-console.log(actualMenu);
 const placeToInsert = document.querySelector('.js-menu');
 actualMenu.forEach(elemMenu => {
     const markup = templateMenu(elemMenu);
     placeToInsert.insertAdjacentHTML('beforeend', markup);
 });
-
+// switching dark/light themes
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 const switcherElem = document.querySelector('.theme-switch__toggle');
 const currentTheme = localStorage.getItem('currentTheme');
+const bodyClassElem = document.body.classList;
+const onChange = function (e) {
+    bodyClassElem.toggle(Theme.DARK);
+    bodyClassElem.toggle(Theme.LIGHT);
+    localStorage.setItem('currentTheme', bodyClassElem[0]);
+}
 if (currentTheme === null) {
     localStorage.setItem('currentTheme', Theme.LIGHT);
-    document.body.classList.add(Theme.LIGHT);
-} else { document.body.classList.add(currentTheme) }
+    bodyClassElem.add(Theme.LIGHT);
+} else { bodyClassElem.add(currentTheme) }
 if (currentTheme === Theme.DARK) {
     switcherElem.checked = true
 }
-
-const onChange = function (e) {
-    document.body.classList.toggle(Theme.DARK);
-    document.body.classList.toggle(Theme.LIGHT);
-    localStorage.setItem('currentTheme', document.body.classList[0]);
-
-}
-
 switcherElem.addEventListener('change', onChange);
 
 
